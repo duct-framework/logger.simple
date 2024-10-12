52663199506aca79d60e3bd85e95efb0850f765d
James Reeves <jreeves@weavejester.com>
2024-10-13 00:28:38 +0100

    Update project file with project information

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject org.duct-framework/logger.simple "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple logger for the Duct framework"
+  :url "https://github.com/duct-framework/logger.simple"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.11.4"]
                  [amalloy/ring-buffer "1.3.1"]
                  [duct/logger "0.3.0"]
-                 [integrant "0.12.0"]]
-  :repl-options {:init-ns logger.simple})
+                 [integrant "0.12.0"]])
