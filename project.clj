8dbedd2a9ab11407a7f4d9aca3daac200d2084a8
James Reeves <jreeves@weavejester.com>
2024-10-24 12:00:24 +0100

    Update Integrant dependency

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.11.4"]
                  [amalloy/ring-buffer "1.3.1"]
                  [org.duct-framework/logger "0.3.0"]
-                 [integrant "0.12.1"]])
+                 [integrant "0.13.0"]])
