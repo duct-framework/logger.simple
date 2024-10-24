90631f71cd13aeafa068aaf1fbbc1c49e3b755a4
James Reeves <jreeves@weavejester.com>
2024-10-24 11:59:34 +0100

    Add tests for filtering by log level

--- test/duct/logger/simple_test.clj
+++ test/duct/logger/simple_test.clj
@@ -1,5 +1,5 @@
 (ns duct.logger.simple-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.test :refer [deftest is testing]]
             [integrant.core :as ig]
             [duct.logger :as logger]
             [duct.logger.simple :as simple]))
@@ -35,6 +35,41 @@
         (ig/halt! system)))
     (is (re-matches
          #"(?x)[0-9TZ.:-]+\s:example/foo\n
-                   [0-9TZ.:-]+\s:example/bar\s\{:x\s1\}\n"
+               [0-9TZ.:-]+\s:example/bar\s\{:x\s1\}\n"
          (slurp tempfile)))
     (.delete tempfile)))
+
+(deftest level-restrict-test
+  (testing "STDOUT logger"
+    (let [system (ig/init {:duct.logger/simple
+                           {:appenders [{:type :stdout
+                                         :levels #{:info}}]}})]
+      (try
+        (let [logger (:duct.logger/simple system)
+              output (java.io.StringWriter.)]
+          (with-redefs [*out* output]
+            (logger/debug logger :example/foo)
+            (logger/info logger :example/bar {:x 1})
+            (Thread/sleep 100))
+          (is (re-matches
+               #"[0-9TZ.:-]+\s:example/bar\s\{:x\s1\}\n"
+               (str output))))
+        (finally
+          (ig/halt! system)))))
+  (testing "file logger"
+    (let [tempfile (java.io.File/createTempFile "logger-test" ".log")
+          temppath (.getAbsolutePath tempfile)
+          system   (ig/init {:duct.logger/simple
+                             {:appenders [{:type :file, :path temppath
+                                           :levels #{:info}}]}})]
+      (try
+        (let [logger (:duct.logger/simple system)]
+          (logger/debug logger :example/foo)
+          (logger/info logger :example/bar {:x 1})
+          (Thread/sleep 100))
+        (finally
+          (ig/halt! system)))
+      (is (re-matches
+           #"[0-9TZ.:-]+\s:example/bar\s\{:x\s1\}\n"
+           (slurp tempfile)))
+      (.delete tempfile))))
