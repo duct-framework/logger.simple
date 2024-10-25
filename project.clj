7ff86864a9ff3f2133193e7d9b8b4f95729ceec5
James Reeves <jreeves@weavejester.com>
2024-10-25 11:37:55 +0100

    Release 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.duct-framework/logger.simple "0.3.0"
+(defproject org.duct-framework/logger.simple "0.4.0"
   :description "A simple logger for the Duct framework"
   :url "https://github.com/duct-framework/logger.simple"
   :license {:name "Eclipse Public License"
